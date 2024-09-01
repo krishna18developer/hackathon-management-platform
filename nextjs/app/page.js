6ede77d847df190996551514c87cc57a11f1ecc7
jitesh <jitesh.kumar05official@gmail.com>
2024-09-01 12:18:38 +0530

    added html to jsx

--- nextjs/app/page.js
+++ nextjs/app/page.js
@@ -3,8 +3,8 @@ import Hero from "./hero/page";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Hero />
+    <main className="flex min-h-screen w-[95vw] flex-col items-center justify-between p-24">
+      <Hero className="w-full" />
     </main>
   );
 }
--- nextjs/app/register/page.js
+++ nextjs/app/register/page.js
@@ -111,5 +111,4 @@ const Register = () => {
     </div>
   )
 }
-
 export default Register
