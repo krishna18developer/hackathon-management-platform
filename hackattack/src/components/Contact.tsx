21283cb5491fda8b6f5222a5c3688f58e2cd1792
Krishna Teja Mekala <krishna18developer@gmail.com>
2024-09-12 11:53:03 +0530

    Updated Timer

--- hackattack/src/components/Contact.tsx
+++ hackattack/src/components/Contact.tsx
@@ -5,8 +5,8 @@ const Contact: React.FC = () => {
     <div id="contact">
       <div>
       <h1 className="section-title">Contact</h1>
-        <p className="section-content"><span>Mahesh</span> - 94947 91201</p>
-        <p className="section-content"><span>Chinmayee</span> - 93463 12543 </p>
+        <p className="section-content"><span>Thanniru Mahesh</span> - 94947 91201</p>
+        <p className="section-content"><span>S. Chinmai Anjali</span> - 93463 12543 </p>
       </div>
     </div>
   );
--- hackattack/src/components/Launch.tsx
+++ hackattack/src/components/Launch.tsx
@@ -8,6 +8,7 @@ const LaunchPage: React.FC = () => {
   const [videoPlayed, setVideoPlayed] = useState(false);
   const [showTimer, setShowTimer] = useState(false);
   const [showLaunchButton, setShowLaunchButton] = useState(true);
+  const [showTitle, setShowTitle] = useState(true); // Added state for controlling title visibility
   const navigate = useNavigate();
 
   const handleLaunch = () => {
@@ -15,10 +16,11 @@ const LaunchPage: React.FC = () => {
     setShowVideo(false);
     setVideoPlayed(false);
     setShowLaunchButton(false);
+    setShowTitle(false); // Hide the title when countdown starts
     setCountdown(5);
 
     const timer = setInterval(() => {
-      setCountdown(prev => {
+      setCountdown((prev) => {
         if (prev === 1) {
           clearInterval(timer);
           setShowVideo(true);
@@ -32,11 +34,14 @@ const LaunchPage: React.FC = () => {
 
   const handleVideoEnd = () => {
     setVideoPlayed(true);
-    setTimeout(() => navigate('/'), 2000); // Delay for smooth transitionxw
+    setTimeout(() => navigate('/'), 2000); // Delay for smooth transition
   };
 
   return (
     <div className="launch-container">
+      {showTitle && (
+        <h1 className="section-title-launch"> Hack Attack 2024 Launch </h1> // Title will disappear after countdown starts
+      )}
       {showVideo ? (
         <div className="video-section">
           <video
